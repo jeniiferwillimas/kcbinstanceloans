d0c66a474a10b2a2fdf02ea93251fcbb2d3836e1
jeth-consultant <sumbeiywetjethro4@gmail.com>
2026-07-04 13:44:58 +0300

    added loan application controller and it route

--- kcb-loan-backend/routes/api.php
+++ kcb-loan-backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoanTypeController;
+use App\Http\Controllers\Api\LoanApplicationController;
 
-
+Route::post('/apply-loan', [LoanApplicationController::class, 'store']);
 Route::get('/loan-types', [LoanTypeController::class, 'index']);
